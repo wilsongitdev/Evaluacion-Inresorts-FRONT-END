48cd0b39f27210ed73632a700dde14dada5a7e6e
wilsongitdev <wilson_13_10@hotmail.com>
2021-06-15 17:34:36 -0500

    first commit

---
+++ src/Common/MainHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainHeader.css';
+function MainHeader(props) {
+    return (
+        <header id="main-header">
+            <div className="container">
+            <h1>In resorts</h1>
+            </div>
+        </header>
+    );
+}
+
+export default MainHeader;
